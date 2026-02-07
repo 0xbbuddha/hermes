b35bb93e01c8dd6d69afc46e4110279ec943bed9
0xbbuddha <killian@archimedeos.org>
2026-02-07 18:46:47 +0100

    fix name

--- Payload_Type/hermes/agent_functions/builder.py
+++ Payload_Type/hermes/agent_functions/builder.py
@@ -35,7 +35,7 @@ Messenger god. Requires Python 3.8+ and pip install cryptography on target.
     agent_path = Path("agent_functions")
     agent_code_path = Path("agent_code")
     # Icon is in agent_functions directory (like Apollo)
-    agent_icon_path = agent_path / "caduceus.svg"
+    agent_icon_path = agent_path / "hermes.svg"
     build_steps = [
         BuildStep(step_name="Gathering Files", step_description="Gathering agent files"),
         BuildStep(step_name="Injecting Config", step_description="Injecting C2 config and UUID"),
